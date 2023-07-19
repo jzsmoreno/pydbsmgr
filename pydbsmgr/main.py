bda334e288b6e535719e693571652787235ece24
Esteban Sanchez <109927620+esanchezmex@users.noreply.github.com>
2023-07-19 16:43:32 -0600

    Docstrings and User-Friendly Variable names
    Added docstrings to the functions and changed the names of the variables to be more user-friendly.

--- pydbsmgr/main.py
+++ pydbsmgr/main.py
@@ -21,13 +21,13 @@ warnings.filterwarnings("ignore")
 ########################################################################################
 
 
-def clean_names(x: str, pattern: str = r"[a-zA-Zñáéíóú_]+\b") -> str:
+def clean_names(dirty_string: str, pattern: str = r"[a-zA-Zñáéíóú_]+\b") -> str:
     """
     Receive a string and clean it of special characters
 
     Parameters
     ----------
-    x : str
+    dirty_string : str
         string of characters
     pattern : str
         regular expression string
@@ -37,57 +37,123 @@ def clean_names(x: str, pattern: str = r"[a-zA-Zñáéíóú_]+\b") -> str:
     result : str
         clean character string
     """
-    result = re.findall(pattern, str(x).replace("_", ""))
+    result = re.findall(pattern, str(dirty_string).replace("_", ""))
     if len(result) > 0:
         result = "_".join(result)
     else:
         pattern = r"[a-zA-Z]+"
-        result = re.findall(pattern, str(x).replace("_", ""))
+        result = re.findall(pattern, str(dirty_string).replace("_", ""))
         result = "_".join(result)
     return result
 
 
-def clean_transform(x: Index, mode: bool = True) -> List[str]:
-    y = []
-    for i in x:
+def clean_transform(col_index: Index, mode: bool = True) -> List[str]:
+    """
+    Transforms a column index by cleaning the column names and if needed makes them capital.
+
+    Parameters
+    ----------
+        col_index : Index
+            The column index to be transformed.
+        mode : bool = True
+            Indicates if names will be capitalized (True as default).
+
+    Returns
+    ----------
+        List : str
+            The transformed column names as a list of strings.
+    """
+    col_name_list = []
+    for col in col_index:
         if mode:
-            y.append(str(clean(i)).title())
+            col_name_list.append(str(clean(col)).title())
         else:
-            y.append(clean(i))
-    return y
+            col_name_list.append(clean(col))
+    return col_name_list
 
 
-def remove_char(x: str) -> str:
+def remove_char(input_string: str) -> str:
+    """
+    Removes special characters from a string.
+
+    Parameters
+    ----------
+        input_string : str
+            The input string from which characters will be removed.
+
+    Returns
+    ----------
+        str : The string with specified characters removed.
+    """
     list_of_char = ["#", "$", "*", "?", "!"]
-    for i in list_of_char:
+    for char in list_of_char:
         try:
-            x = x.replace(i, "")
+            input_string = input_string.replace(char, "")
         except:
-            return x
-    return x
+            return input_string
+    return input_string
 
 
-def check_if_isemail(x: str) -> str:
-    find_ = False
-    if str(x).find("@") != -1:
-        x = str(clean(x))
-        find_ = True
+def check_if_isemail(check_email: str) -> str:
+    """
+    Checks if a string is an email address and returns the cleaned string and a flag indicating if the string is an email.
 
-    return x, find_
+    Parameters
+    ----------
+        check_email : str
+            The input string to be checked for an email address.
+
+    Returns
+    ----------
+        Tuple : str, bool
+            A tuple containing the cleaned string and a boolean flag indicating if an email address was found.
+    """
+    found_email = False
+    if str(check_email).find("@") != -1:
+        check_email = str(clean(check_email))
+        found_email = True
+
+    return check_email, found_email
 
 
-def convert_date(x: str) -> str:
+def convert_date(date_string: str) -> str:
+    """
+    Converts a string of a date to a proper date format.
+
+    Parameters
+    ----------
+    date_string : str
+        The input string representing a date.
+
+    Returns
+    -------
+    proper_date : str
+        The date string in the proper format 'YYYY-MM-DD'.
+    """
     try:
-        x = str(pd.to_datetime(x, format="%Y%m%d", errors="raise"))[:10]
+        proper_date = str(pd.to_datetime(date_string, format="%Y%m%d", errors="raise"))[:10]
     except:
         try:
-            x = str(pd.to_datetime(x, format="%d%m%Y", errors="raise"))[:10]
+            proper_date = str(pd.to_datetime(date_string, format="%d%m%Y", errors="raise"))[:10]
         except:
-            x = str(pd.to_datetime(x, format="%Y%m%d", errors="ignore"))[:10]
-    return x
+            proper_date = str(pd.to_datetime(date_string, format="%Y%m%d", errors="ignore"))[:10]
+    return proper_date
 
 
 def clean_and_convert_to(x: str) -> str:
+    """
+    Performs cleaning and some conversions on a string.
+
+    Parameters
+    ----------
+    x : str
+        The input string to be cleaned and converted.
+
+    Returns
+    -------
+    str
+        The cleaned and converted string.
+    """
     # pattern_to_year = r"\d{4}"
     x = remove_char(x)
     try:
@@ -118,40 +184,81 @@ def clean_and_convert_to(x: str) -> str:
     return x
 
 
-def correct_nan(x: str) -> str:
-    if str(x).find("nan") != -1:
+def correct_nan(check_missing: str) -> str:
+    """
+    Corrects the format of missing values in a string to the correct numpy.nan.
+
+    Parameters
+    ----------
+    check_missing : str
+        The string to be checked for incorrect missing value format.
+
+    Returns
+    -------
+    str
+        The corrected string format.
+    """
+    if str(check_missing).find("nan") != -1:
         return np.nan
     else:
-        return x
+        return check_missing
 
 
-def check_dtypes(df: DataFrame, datatypes: Series) -> DataFrame:
-    cols = df.columns
-    j = 0
-    for i in datatypes:
-        if i == "object":
-            df[cols[j]] = df[cols[j]].apply(clean_and_convert_to)
-            df[cols[j]] = df[cols[j]].apply(correct_nan)
+def check_dtypes(dataframe: DataFrame, datatypes: Series) -> DataFrame:
+    """
+    Checks and updates the data types of columns in a DataFrame.
+
+    Parameters
+    ----------
+    dataframe : DataFrame
+        The DataFrame to check and update the data types.
+    datatypes : Series
+        The Series containing the desired data types for each column in the DataFrame.
+
+    Returns
+    -------
+    DataFrame
+        The DataFrame with updated data types.
+    """
+    cols = dataframe.columns
+    column_index = 0
+    for datatype in datatypes:
+        if datatype == "object":
+            dataframe[cols[column_index]] = dataframe[cols[column_index]].apply(clean_and_convert_to)
+            dataframe[cols[column_index]] = dataframe[cols[column_index]].apply(correct_nan)
             try:
-                df[cols[j]] = df[cols[j]].map(str.strip)
+                dataframe[cols[column_index]] = dataframe[cols[column_index]].map(str.strip)
             except:
                 try:
-                    df[cols[j]] = df[cols[j]].astype("datetime64[ns]")
+                    dataframe[cols[column_index]] = dataframe[cols[column_index]].astype("datetime64[ns]")
                 except:
                     None
-        j += 1
-    return df
+        column_index += 1
+    return dataframe
 
 
 def create_yaml_tree(yaml_name: str, df_info: DataFrame) -> None:
-    """Function that creates a yaml configuration file for database data type validation."""
+    """
+    Function that creates a yaml configuration file for database data type validation.
+
+    Parameters
+    ----------
+    yaml_name : str
+        The name of the yaml configuration file to be created.
+    df_info : DataFrame
+        The DataFrame with the column information for data type validation.
+
+    Returns
+    -------
+    Yaml file with the name given.
+    """
     data = {}
-    for name, t, sql_name in zip(
+    for col_name, data_type, sql_name in zip(
         df_info["column name"].to_list(),
         df_info["data type"],
         df_info["sql name"].to_list(),
     ):
-        data[name] = {"type": [t], "sql_name": [sql_name]}
+        data[col_name] = {"type": [data_type], "sql_name": [sql_name]}
 
     yaml_data = yaml.dump({"database": data})
 
@@ -162,6 +269,27 @@ def create_yaml_tree(yaml_name: str, df_info: DataFrame) -> None:
 def check_values(
     df_: DataFrame, df_name: str, sheet_name: str, mode: bool = True, cols_upper_case: bool = False
 ) -> Tuple[DataFrame, DataFrame]:
+    """
+    Perform data validation on a DataFrame and create a Data Quality Assesment Report on the DataFrame.
+
+    Parameters
+    ----------
+    df_ : DataFrame
+        The DataFrame to be validated.
+    df_name : str
+        The name of the DataFrame.
+    sheet_name : str
+        The name of the excel sheet within the DataFrame.
+    mode : bool, optional
+        Indicates whether to generate a visualization and report in html, True by default.
+    cols_upper_case : bool, optional
+        Indicates whether to convert column names to uppercase, False by default.
+
+    Returns
+    -------
+    info, df : DataFrame, DataFrame
+        A tuple containing the information DataFrame and the validated DataFrame.
+    """
     df = df_.copy()
     df.columns = clean_transform(df.columns, cols_upper_case)
     df = check_dtypes(df, df.dtypes)
@@ -175,17 +303,17 @@ def check_values(
         # profile.to_file("./Detail of the report/" + title + ".html")
         ax = msno.matrix(df)
         ax.get_figure().savefig("./Detail of the report/" + title + ".png", dpi=300)
-    for i in df.columns:
+    for col in df.columns:
         try:
-            if i.find("unnamed") == -1:
-                nrows = df.isnull().sum()[i] + df[i].count()
-                nrows_missing = df.isnull().sum()[i]
+            if col.find("unnamed") == -1:
+                nrows = df.isnull().sum()[col] + df[col].count()
+                nrows_missing = df.isnull().sum()[col]
                 percentage = nrows_missing / nrows
-                datatype = df.dtypes[i]
-                unique_vals = len(df[i].unique())
+                datatype = df.dtypes[col]
+                unique_vals = len(df[col].unique())
                 info.append(
                     [
-                        i,
+                        col,
                         datatype,
                         df_name,
                         sheet_name,
@@ -195,7 +323,7 @@ def check_values(
                         unique_vals,
                     ]
                 )
-                # print(str(i)+' : 'f"{(percentage)*100:.2f}%")
+                # print(str(col)+' : 'f"{(percentage)*100:.2f}%")
         except:
             None
     info = np.array(info).reshape((-1, 8))
@@ -227,9 +355,6 @@ def check_for_list(
     dfs_names: List[str],
     sheet_names: List[str],
     mode: bool = True,
-    cols_upper_case: bool = False,
-    quality_report: bool = True,
-    yaml_dtype: bool = True,
     yaml_name: str = "./output.yaml",
     report_name: str = "./report-health-checker.html",
     encoding: str = "latin1",
