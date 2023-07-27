2c7045796bdcfd36831739c9f03c6a60854fa64b
Esteban Sanchez <109927620+esanchezmex@users.noreply.github.com>
2023-07-27 16:44:51 -0600

    Added check_column_types function to tools.

--- pydbsmgr/utils/tools.py
+++ pydbsmgr/utils/tools.py
@@ -2,6 +2,67 @@ import os
 
 import yaml
 
+def check_column_types(df: DataFrame, drop_rows: bool = True, int_replace: int = -1) -> DataFrame:
+    """
+    Check and correct the data types of columns in a `DataFrame`.
+
+    Parameters
+    ----------
+    df : `DataFrame`
+        The input `DataFrame` to check and correct data types.
+    drop_rows : `bool`, optional
+        If `True`, rows with incorrect data types will be dropped, by default `True`.
+    int_replace : `int`, optional
+        The value used to replace incorrect integer values if `drop_rows` is `False`, by default -1.
+
+    Returns
+    -------
+    DataFrame
+        A new `DataFrame` with corrected data types or dropped rows based on the options chosen.
+    """
+    def check_float(x):
+        if isinstance(x, str):
+            try:
+                return float(x)
+            except:
+                return np.nan
+
+    def check_int(x):
+        if isinstance(x, str):
+            try:
+                return int(x)
+            except:
+                return np.nan
+
+    df_ = df.copy()
+    dict_dtypes = dict(zip(["float", "int", "str"], ["float64", "int64", "object"]))
+    for col in df_.columns:
+        col_dtype = df_[col].dtype
+        col_samples = df_[col].sample(n=round(len(df_[col]) * 0.01))
+        for sample in col_samples:
+            val_dtype = type(sample).__name__
+            if not dict_dtypes[val_dtype] == col_dtype:
+                # print(f'The dtype of the sample {sample} is {val_dtype} and does not match the dtype of the column {col} which is {col_dtype}')
+                df_[col] = df_[col].replace("nan", np.nan)
+
+                if val_dtype == "float":
+                    df_[col] = df_[col].apply(check_float)
+                    # if drop_rows:
+                    #     df_ = df_.loc[df_[col].notnull()]
+                    # else:
+                    #     df_[col] = df_[col].fillna(int_replace)
+                    df_[col] = df_[col].astype(dict_dtypes[val_dtype])
+
+                if val_dtype == "int":
+                    df_[col] = df_[col].apply(check_int)
+                    if drop_rows:
+                        df_ = df_.loc[df_[col].notnull()]
+                    else:
+                        df_[col] = df_[col].fillna(int_replace)
+                    df_[col] = df_[col].astype(dict_dtypes[val_dtype])
+    return df_
+
+
 
 def create_directory(data, parent_path=""):
     """
