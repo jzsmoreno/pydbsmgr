5bed8412ea6a9e6873fdfe155079d06bddab863a
BubuDavid <david.pedroza.segoviano@gmail.com>
2023-11-24 20:50:41 +0000

    Add some rewrites to sql functions

--- pydbsmgr/utils/sql_functions.py
+++ pydbsmgr/utils/sql_functions.py
@@ -65,7 +65,9 @@ class FileToSQL(DataFrameToSQL):
         # txt = "{:,}".format(len(df))
         print(f"Will be loaded {len(df_to_load)} rows.")
         if overwrite:
-            self._create(df_to_load, table_name, overwrite, char_length, override_length)
+            self._create(
+                df_to_load, table_name, overwrite, char_length, override_length
+            )
             self._append_to_table(df_to_load.iloc[2:, :], table_name)
         else:
             self._append_to_table(df_to_load.iloc[2:, :], table_name)
@@ -77,8 +79,16 @@ class FileToSQL(DataFrameToSQL):
         overwrite: bool,
         char_length: int,
         override_length: bool,
+        close_cursor: bool = True,
     ):
-        self.import_table((df.iloc[0:1, :]), table_name, overwrite, char_length, override_length)
+        self.import_table(
+            (df.iloc[0:1, :]),
+            table_name,
+            overwrite,
+            char_length,
+            override_length,
+            close_cursor,
+        )
 
     def _append_to_table(self, df: DataFrame, db_table_nm: str) -> None:
         # csv_buffer = StringIO()
@@ -111,7 +121,7 @@ class FileToSQL(DataFrameToSQL):
 
     def bulk_insert_from_csv(
         self,
-        file_name: str,
+        file_path: str,
         db_table_name: str,
         sas_str: str,
         storage_connection_string: str,
@@ -137,67 +147,90 @@ class FileToSQL(DataFrameToSQL):
         ----------
             `bool`: True if the data was inserted successfully
         """
-        # Checking file type
-        df: DataFrame = pd.DataFrame()
-        is_parquet = file_name.endswith("parquet")
-        try:
-            df_list = self.write_csv_from_parquet(
-                storage_connection_string=storage_connection_string,
-                container_name=container_name,
-                directory="/".join(file_name.split("/")[:-1]),
-                filter_condition=file_name.split("/")[-1].replace(".csv", ".parquet"),
-                write_to_csv=is_parquet,
-            )
-        except Exception as e:
-            print("==============================================")
-            warning_type = "UserWarning"
-            msg = "Parquet asociated with the csv file not found."
-            msg += f"Error: {e}"
-            print(f"{warning_type}: {msg}")
-            print("==============================================")
+        # Get all the files in the container or file individually
+        filter_condition = ""
+        if not file_path.endswith("/"):
+            filter_condition = file_path.split("/")[-1]
 
-            df_list, _ = self.storage_controller.get_excel_csv(
-                directory_name="/".join(file_name.split("/")[:-1]), regex="\w+.csv"
-            )
+        print("GETTING FILES FROM THE CONTAINER")
+        df_list, name_list = self.get_df(
+            file_path=file_path,
+            storage_connection_string=storage_connection_string,
+            container_name=container_name,
+            filter_condition=filter_condition,
+        )
+        print("FINISHED GETTING FILES FROM THE CONTAINER")
+        if not df_list:
+            print("No files found")
+            return False
 
         df = df_list[0]
-        # Create master key
+        print("=============================================")
+        if file_path.endswith(".parquet"):
+            # Write the csv files from df_list and name_list
+            print("WRITING CSV FILES")
+            self.write_csv_from_parquet(
+                df_list=df_list,
+                name_list=name_list,
+                directory="/".join(file_path.split("/")[:-1]),
+            )
+            print("FINISHED WRITING CSV FILES")
+
+        # Create master key if not created already
+        print("CREATING MASTER KEY")
         try:
             sspassword = generate_secure_password()
-            self._cur.execute(f"CREATE MASTER KEY ENCRYPTION BY PASSWORD = '{sspassword}'")
+            self._cur.execute(
+                f"CREATE MASTER KEY ENCRYPTION BY PASSWORD = '{sspassword}'"
+            )
+            print(f"Master key created")
         except pyodbc.ProgrammingError as e:
             print("==============================================")
             warning_type = "UserWarning"
             msg = f"Master key already exists. If you want to create a new one, please drop the existing one first."
             msg += f"Error: {e}"
             print(f"{warning_type}: {msg}")
-            print("==============================================")
+        print("=============================================")
 
         # Create credentials query
+        print("CREATING CREDENTIALS")
         try:
             credentials_query = f"""CREATE DATABASE SCOPED CREDENTIAL {credential_name}
             WITH IDENTITY = 'SHARED ACCESS SIGNATURE',
             SECRET = '{sas_str}';"""
             self._cur.execute(credentials_query)
+            print(f"Credentials with name {credential_name} created")
         except pyodbc.ProgrammingError as e:
             print("==============================================")
             warning_type = "UserWarning"
             msg = f"Credentials already exists. If you want to create a new one, please drop the existing one first."
             msg += f"Error: {e}"
             print(f"{warning_type}: {msg}")
-            print("==============================================")
+        print("=============================================")
 
         # Create external data source query
-        external_data_source_query = f"""CREATE EXTERNAL DATA SOURCE {data_source_name}
-        WITH (
-            TYPE = BLOB_STORAGE,
-            LOCATION = 'https://{storage_account}.blob.core.windows.net',
-            CREDENTIAL = {credential_name}
-        );"""
+        print("CREATING EXTERNAL DATA SOURCE")
+        try:
+            external_data_source_query = f"""CREATE EXTERNAL DATA SOURCE {data_source_name}
+            WITH (
+                TYPE = BLOB_STORAGE,
+                LOCATION = 'https://{storage_account}.blob.core.windows.net',
+                CREDENTIAL = {credential_name}
+            );"""
+            self._cur.execute(external_data_source_query)
+            print(f"External data source with name {data_source_name} created")
+        except pyodbc.ProgrammingError as e:
+            print("==============================================")
+            warning_type = "UserWarning"
+            msg = f"External data source already exists. If you want to create a new one, please drop the existing one first."
+            msg += f"Error: {e}"
+            print(f"{warning_type}: {msg}")
+
+        print("=============================================")
 
         # Create bulk insert query
         bulk_insert_query = f"""BULK INSERT {db_table_name}
-        FROM '{container_name}/{file_name}'
+        FROM '{file_path}'
         WITH (
             DATA_SOURCE = '{data_source_name}',
             FIELDTERMINATOR = ',',
@@ -206,29 +239,31 @@ class FileToSQL(DataFrameToSQL):
             FIRSTROW = 3
         );"""
 
-        try:
-            self._cur.execute(external_data_source_query)
-        except pyodbc.ProgrammingError as e:
-            print("==============================================")
-            warning_type = "UserWarning"
-            msg = f"External data source already exists. If you want to create a new one, please drop the existing one first."
-            msg += f"Error: {e}"
-            print(f"{warning_type}: {msg}")
-            print("==============================================")
         # Create table
         if overwrite:
             print("CREATING TABLE FOR BULK INSERT")
-            self._create(df, db_table_name, overwrite, char_length, True)
-            # Create connection and cursor again
-            self._con = pyodbc.connect(self._connection_string, autocommit=True)
-            self._cur = self._con.cursor()
+            self._create(
+                df=df,
+                table_name=db_table_name,
+                overwrite=overwrite,
+                char_length=char_length,
+                override_length=True,
+                close_cursor=False,
+            )
+            print("TABLE CREATED")
+            print("=============================================")
         print("INSERTING DATA, PLEASE WAIT")
         self._cur.execute(bulk_insert_query)
-        # Drop dropable objects
-        self.drop_dropables(data_source_name, credential_name=credential_name, masterkey=True)
-        self._con.close()
-        print("Successfully 'Bulk Inserted'")
+        print("SUCCESSFULLY 'BULK INSERTED'")
+        print("=============================================")
 
+        # Drop dropable objects
+        self.drop_dropables(
+            data_source_name, credential_name=credential_name, masterkey=True
+        )
+        self._con.close()
+
+        print("PROCESS FINISHED")
         return True
 
     def drop_dropables(
@@ -262,14 +297,54 @@ class FileToSQL(DataFrameToSQL):
 
         return True
 
+    def get_df(
+        self,
+        file_path: str,
+        storage_connection_string: str,
+        container_name: str,
+        filter_condition: str,
+    ) -> tuple[list[DataFrame], list[str]]:
+        self.storage_controller = StorageController(
+            storage_connection_string, container_name
+        )
+        file_names = self.storage_controller.get_all_blob(filter_criteria=file_path)
+        filter_names = file_names
+        if filter_condition != "":
+            filter_names = self.storage_controller._list_filter(
+                file_names, filter_condition
+            )
+
+        self.storage_controller.set_BlobPrefix(filter_names)
+        if file_path.endswith(".parquet"):
+            # Get all the files in the container
+            print("GETTING PARQUET FILES FROM THE CONTAINER")
+            df_list, name_list = self.storage_controller.get_parquet(
+                directory_name="/".join(file_path.split("/")[:-1]),
+                regex="\w+.parquet",
+                manual_mode=True,
+            )
+            return df_list, name_list
+        elif file_path.endswith(".csv"):
+            # Get all the files in the container
+            print("GETTING CSV FILES FROM THE CONTAINER")
+            df_list, name_list = self.storage_controller.get_excel_csv(
+                directory_name="/".join(file_path.split("/")[:-1]),
+                regex="\w+.csv",
+                manual_mode=True,
+            )
+            return df_list, name_list
+        else:
+            print("Not supported yet")
+
+        return [], []
+
     def write_csv_from_parquet(
         self,
-        storage_connection_string: str,
-        container_name: str = "/",
+        df_list: list[DataFrame],
+        name_list: list[str],
         directory: str = "/",
-        filter_condition: str = "",
         write_to_csv: bool = True,
-    ) -> list[DataFrame]:
+    ) -> None:
         """Write a csv file from parquet files in a container
         Parameters:
         ----------
@@ -280,18 +355,6 @@ class FileToSQL(DataFrameToSQL):
         ----------
             `bool`: True if the file was created successfully
         """
-        # Get all the files in the container
-        print("GETTING PARQUET FILES FROM THE CONTAINER")
-        self.storage_controller = StorageController(storage_connection_string, container_name)
-        file_names = self.storage_controller.get_all_blob(filter_criteria=directory)
-        filter_names = file_names
-        if filter_condition != "":
-            filter_names = self.storage_controller._list_filter(file_names, filter_condition)
-
-        self.storage_controller.set_BlobPrefix(filter_names)
-
-        df_list, name_list = self.storage_controller.get_parquet("/", "\w+.parquet", True)
-
         # Write the csv files
         if write_to_csv:
             print("WRITING CSV FILES")
@@ -302,7 +365,6 @@ class FileToSQL(DataFrameToSQL):
             )
 
             print("FINISHED WRITING CSV FILES")
-        return df_list
 
 
 ########################################################################################
